d65fa9124577072a56301511c0ccb926636764f0
Raed <xraed20@gmail.com>
2025-04-13 18:14:01 +0300

    try add data

--- eTeamsHub/eTeamsHub/settings.py
+++ eTeamsHub/eTeamsHub/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,22 +85,16 @@ USE_RAILWAY_DB = os.environ.get("RAILWAY") == "True"
 
 if USE_RAILWAY_DB:
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ.get("PGDATABASE"),
-            'USER': os.environ.get("PGUSER"),
-            'PASSWORD': os.environ.get("PGPASSWORD"),
-            'HOST': os.environ.get("PGHOST"),
-            'PORT': os.environ.get("PGPORT"),
-        }
+        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
     }
 else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / "db.sqlite3",
+            'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
+
 
 
 # Password validation
