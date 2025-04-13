edd6500cf218831f65532c1b2b32d4bfcd5d9a52
Raed <xraed20@gmail.com>
2025-04-13 17:42:03 +0300

    test db

--- eTeamsHub/main/urls.py
+++ eTeamsHub/main/urls.py
@@ -3,5 +3,6 @@ from . import views
 app_name = "main"
 
 urlpatterns = [
-    path("" ,views.home,name="home")
+    path("" ,views.home,name="home"),
+    path("test-db/", views.test_db_view),
 ]
--- eTeamsHub/main/views.py
+++ eTeamsHub/main/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from teams.models import Team
 from games.models import Game
+import django.db
+from django.http import HttpRequest,HttpResponse
 
 def home(request):
     teams = Team.objects.all()[:3]
     games = Game.objects.all()[:3]
 
     return render(request, "main/home.html", {"teams": teams,"games": games,})
+
+def test_db_view(request):
+    db = django.db.connection.settings_dict
+    return HttpResponse(f"Using DB: {db['ENGINE']} - {db['NAME']}")
